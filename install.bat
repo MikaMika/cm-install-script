3aadb442e2c6d31b4ed9b081d449c499ae1ee679
Michaël Dima <maniacmika@gmail.com>
2025-10-30 23:24:31 +0100

    Add security checks

--- install.bat
+++ install.bat
@@ -1,52 +1,51 @@
-:: Deleting all non-necessary files to start fresh
-md d
-move .\* .\d
-move d\curl.exe .
-move d\fixpath.bat .
-move d\install.bat .
-move d\lucon.ttf .
-move d\msvcp140.dll .
-move d\tar.exe .
-del /s /q d
-rd /s /q d
-
-:: Downloading all required files
-if not exist curl.exe      (echo curl is missing. This is a requirement for this script. Extract the entirety of the zip file containing this script. & pause & exit)
-if not exist tar.exe       (echo tar is missing. This is a requirement for this script. Extract the entirety of the zip file containing this script. & pause & exit)
-if not exist cm.zip        (.\curl -Lo cm.zip   "https://gitlab.com/the-no-frauds-club/cosmonarchy-bw-prerelease/-/archive/main/cosmonarchy-bw-prerelease-main.zip")
-if not exist maps.zip      (.\curl -Lo maps.zip "https://gitlab.com/the-no-frauds-club/cmbw-root/-/archive/main/cmbw-root-main.zip")
-if not exist bw.zip        (.\curl -Lo bw.zip   "https://fraudsclub.com/files/starcraft.zip")
-if not exist msvcp140.dll  (.\curl -LO          "https://mikamika.top/cm/dl/msvcp140.dll")
-if not exist lucon.ttf     (.\curl -LO          "https://mikamika.top/cm/dl/lucon.ttf")
-if not exist fixpath.bat   (.\curl -LO          "https://mikamika.top/cm/dl/fixpath.bat")
-if not exist cnc-ddraw.zip (.\curl -LO          "https://github.com/FunkyFr3sh/cnc-ddraw/releases/download/v7.0.0.0/cnc-ddraw.zip")
-if not exist CrownLink.snp (.\curl -LO          "https://github.com/impromptu1583/CrownLink/releases/latest/download/CrownLink.snp")
-
-:: Setting up Cosmonarchy
+@echo off
+echo.
+echo :: Checking directory name is Cosmonarchy with no subdirectory
+for %%I in (.) do set CurrDirName=%%~nxI
+IF /I NOT "%CurrDirName%" == "cosmonarchy" (echo This script will only run from a directory named cosmonarchy. Verify you are in the correct location or rename accordingly. & pause & exit)
+for /d %%a in (.\*) do (echo Directory detected. This script should only be run as a fresh new install. Extract the entirety of the zip file containing this script then try again. & pause & exit)
+echo.
+echo :: Downloading all required files
+if not exist curl.exe      (echo curl is missing. This is a requirement for this script. Extract the entirety of the zip file containing this script then try again. & pause & exit)
+if not exist tar.exe       (echo tar is missing. This is a requirement for this script. Extract the entirety of the zip file containing this script then try again. & pause & exit)
+if not exist cm.zip        (echo cm    & .\curl -Lo cm.zip   "https://gitlab.com/the-no-frauds-club/cosmonarchy-bw-prerelease/-/archive/main/cosmonarchy-bw-prerelease-main.zip")
+if not exist maps.zip      (echo maps  & .\curl -Lo maps.zip "https://gitlab.com/the-no-frauds-club/cmbw-root/-/archive/main/cmbw-root-main.zip")
+if not exist bw.zip        (echo bw    & .\curl -Lo bw.zip   "https://fraudsclub.com/files/starcraft.zip")
+if not exist msvcp140.dll  (echo msvc  & .\curl -LO          "https://mikamika.top/cm/dl/msvcp140.dll")
+if not exist lucon.ttf     (echo lucon & .\curl -LO          "https://mikamika.top/cm/dl/lucon.ttf")
+if not exist fixpath.bat   (echo path  & .\curl -LO          "https://mikamika.top/cm/dl/fixpath.bat")
+if not exist cnc-ddraw.zip (echo ddraw & .\curl -LO          "https://github.com/FunkyFr3sh/cnc-ddraw/releases/download/v7.0.0.0/cnc-ddraw.zip")
+if not exist CrownLink.snp (echo cl    & .\curl -LO          "https://github.com/impromptu1583/CrownLink/releases/latest/download/CrownLink.snp")
+echo.
+echo :: Setting up Cosmonarchy
 .\tar xf cm.zip
-ren cosmonarchy-bw-* cm
-move cm\* . /y 2>nul
+move cosmonarchy-bw-* cm
+for %%a in (cm\*) do move /y "%%~fa" .
+for /d %%a in (cm\*) do move /y "%%~fa" .
 rd cm
-
-:: Setting up Brood War
+echo.
+echo :: Setting up Brood War
 .\tar xf bw.zip
-move Starcraft\* . /y 2>nul
-rd Starcraft
+move Starcraft sc
+for %%a in (sc\*) do move /y "%%~fa" .
+for /d %%a in (sc\*) do move /y "%%~fa" .
+rd sc
 reg add "HKLM\Software\Wow6432Node\Blizzard Entertainment\Starcraft" /V "Program" /D "%CD%\StarCraft.exe" /F
-
-:: Setting up Maps
+echo.
+echo :: Setting up Maps
 .\tar xf maps.zip
 md maps
 move cmbw-root* maps\cmbw-root
-
-:: Setting up misc
+echo.
+echo :: Setting up misc
 .\tar xf cnc-ddraw.zip
 reg add "HKCU\Software\Wine\DllOverrides" /V "ddraw" /D "native,builtin" /F
 if not exist %SystemRoot%\Fonts\lucon.ttf (copy lucon.ttf %SystemRoot%\Fonts)
-del *.zip
-:: Video Settings
+echo.
+echo :: Video Settings
 "cnc-ddraw config"
-:: Creating data file...
-:: Please wait...
+echo.
+echo :: Creating data file...
+echo :: Please wait...
 "Cosmonarchy BW"
-
+del bw.zip cm.zip maps.zip cnc-ddraw.zip
